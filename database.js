42be86df88fd2581d09e60595a42b78a577038dc
Troy Mullaney <troym9731@gmail.com>
2015-12-01 21:44:49 -0700

    Finished tutorial

---
+++ database.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Superhero = new Schema(
+  { name: String }
+);
+
+mongoose.model('superheros', Superhero);
+
+mongoose.connect('mongodb://localhost/node-superhero');
---
+++ routes/api.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Superhero = mongoose.model('superheros');
+
+router.get('/superheros', function(req, res) {
+  Superhero.find(function(err, superheros) {
+    res.render(
+      'api',
+      {title: 'Superhero API', superheros: superheros}
+    );
+  });
+});
+
+router.get('/superhero/:id', function(req, res) {
+  var query = { "_id": req.params.id };
+  Superhero.findOne(query, function(err, superhero) {
+    res.render(
+      'superhero',
+      {title: 'Superhero API - ' + superhero.name, superhero: superhero}
+    );
+  });
+});
+
+router.post('/superheros', function(req, res) {
+  new Superhero({ name: req.body.name })
+  .save(function(err, superhero) {
+    res.redirect('/api/superheros');
+  });
+});
+
+router.put('/superhero/:id', function(req, res) {
+  var query = { "_id": req.params.id };
+  var update = { name: req.body.name };
+  var options = { new: true };
+  Superhero.findOneAndUpdate(query, update, options, function(err, superhero) {
+    res.render(
+      'superhero',
+      {title: 'Superhero API - ' + superhero.name, superhero: superhero}
+    );
+  });
+});
+
+router.delete('/superhero/:id', function(req, res) {
+  var query = { "_id": req.params.id };
+  Superhero.findOneAndRemove(query, function(err, superhero) {
+    res.redirect('/api/superheros');
+  });
+});
+
+module.exports = router;
